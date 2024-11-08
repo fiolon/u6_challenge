1fe2d93850e33eaf5d940f46f0dcca1dedc08a5b
fiolon <fnarciso@academic.rrc.ca>
2024-11-08 01:05:54 -0600

    Added partial for displaying customer information.

--- config/initializers/ransack_active_storage.rb
+++ config/initializers/ransack_active_storage.rb
@@ -1,13 +1,10 @@
-# config/initializers/ransack_active_storage.rb
 Rails.application.config.to_prepare do
-  # Define ransackable attributes for ActiveStorage::Attachment
   ActiveStorage::Attachment.class_eval do
     def self.ransackable_attributes(auth_object = nil)
       ["blob_id", "created_at", "id", "name", "record_id", "record_type"]
     end
   end
 
-  # Define ransackable attributes for ActiveStorage::Blob
   ActiveStorage::Blob.class_eval do
     def self.ransackable_attributes(auth_object = nil)
       ["content_type", "created_at", "filename", "id", "metadata", "service_name", "byte_size"]

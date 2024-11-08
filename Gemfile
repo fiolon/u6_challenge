7845afbc4ed732d7e033ccc1e3117566280b4aed
fiolon <fnarciso@academic.rrc.ca>
2024-11-07 23:09:16 -0600

    Fixed issues with migrations, also seeded new AdminUser.

--- Gemfile
+++ Gemfile
@@ -8,6 +8,8 @@ gem 'cancancan'
 gem 'draper'
 gem 'pundit'
 
+# Gemfile
+gem 'sassc-rails'
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem "rails", "~> 7.2.1"
 # The original asset pipeline for Rails [https://github.com/rails/sprockets-rails]
--- app/admin/customers.rb
+++ app/admin/customers.rb
@@ -1,18 +1,38 @@
 ActiveAdmin.register Customer do
 
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-  # permit_params :full_name, :phone_number, :email, :notes
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:full_name, :phone_number, :email, :notes]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
-  
+  permit_params :full_name, :phone_number, :email, :notes
+
+  index do
+    selectable_column
+    column "Customers", :title
+    column :full_name
+    column :phone_number
+    column :email
+    column :notes
+    actions
+  end
+
+  form do |f|
+    f.inputs 'Customer Details' do
+      f.input :full_name
+      f.input :phone_number
+      f.input :email
+      f.input :notes
+    end
+    f.actions
+  end
+
+  show do
+    attributes_table do
+      row :id
+      row :full_name
+      row :phone_number
+      row :email
+      row :notes
+      row :created_at
+      row :updated_at
+    end
+    active_admin_comments
+  end
+
 end
